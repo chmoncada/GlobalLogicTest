1522a8571073552e81591b59864667915b625053
Charles Moncada <chmoncada@me.com>
2018-08-31 17:55:33 -0300

    Add `Laptop` model

---
+++ GlobalLogicTest/Model/Laptop.swift
@@ -0,0 +1,15 @@
+//
+//  Laptop.swift
+//  GlobalLogicTest
+//
+//  Created by Charles Moncada on 31/08/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import Foundation
+
+struct Laptop: Codable {
+	let title: String
+	let description: String
+	let imageURL: URL?
+}
